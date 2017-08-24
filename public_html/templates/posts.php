2843097a94a2a297a2937831fce70f05d2e29ee9
Dylan Reuter <djreuter45@gmail.com>
2017-08-24 11:38:37 -0600

    added footer template, html, etc.

--- public_html/templates/posts.php
+++ public_html/templates/posts.php
@@ -53,4 +53,18 @@
 			</div>
 		</main>
 	</div>
+
+	<footer class="navbar-fixed-bottom">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				=^. .^=
+			</div>
+			<div class="col-sm-6 text-right-sm">
+				Meow!
+			</div>
+		</div>
+	</div>
+	</footer>
 </div>
+
